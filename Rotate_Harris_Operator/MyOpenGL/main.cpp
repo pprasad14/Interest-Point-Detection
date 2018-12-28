1a0c48adb507c3a51f7cf27bee25d6d3e22beb43
Prem Prasad <prem.p@cumulations.com>
2018-12-28 18:59:31 +0100

    GUI rendering update 1

---
+++ Rotate_Harris_Operator/MyOpenGL/main.cpp
@@ -0,0 +1,36 @@
+// main.cpp
+
+#include <QApplication>
+#include <QDesktopWidget>
+#include<iostream>
+
+#include "window.h"
+using std::cout;
+using std::endl;
+using std::cin;
+using std::cerr;
+
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    Window window;
+    window.resize(window.sizeHint());
+    int desktopArea = QApplication::desktop()->width() *
+                     QApplication::desktop()->height();
+    int widgetArea = window.width() * window.height();
+
+    window.setWindowTitle("OpenGL with Qt");
+
+    cout << endl << "hello" << endl;
+
+    if (((float)widgetArea / (float)desktopArea) < 0.75f)
+        window.show();
+    else
+        window.showMaximized();
+    return app.exec();
+
+
+
+}
---
+++ Rotate_Harris_Operator/MyOpenGL/myglwidget.h
@@ -0,0 +1,96 @@
+// myglwidget.h
+
+#ifndef MYGLWIDGET_H
+#define MYGLWIDGET_H
+
+#include <QGLWidget>
+
+
+//*************************************************
+#include "window.h"
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+//std::string line;
+//using std::cout;
+//using std::endl;
+//using std::cin;
+//using std::cerr;
+
+//std::ifstream inFile_vert("C:\\Users\\Prem Prasad\\Desktop\\MAIA Projects\\Software Engineering\\OFF files\\cat0.vert");
+//std::ifstream inFile_face("C:\\Users\\Prem Prasad\\Desktop\\MAIA Projects\\Software Engineering\\OFF files\\cat0.tri");
+
+struct vertex {
+    float x;
+    float y;
+    float z;
+};
+
+struct face {
+    int v1;
+    int v2;
+    int v3;
+};
+
+////vertex *vertices;
+////face *faces;
+
+////*************************************************
+
+
+class MyGLWidget : public QGLWidget
+{
+    Q_OBJECT
+public:
+    vertex *vertices;
+    face *faces;
+
+    int no_of_vertices;
+    int no_of_faces;
+
+    void read_points();
+
+    explicit MyGLWidget(QWidget *parent = 0);
+    ~MyGLWidget();
+signals:
+
+public slots:
+
+protected:
+
+    void initializeGL();
+    void paintGL();
+    void resizeGL(int width, int height);
+
+    QSize minimumSizeHint() const;
+    QSize sizeHint() const;
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+
+public slots:
+    // slots for xyz-rotation slider
+    void setXRotation(int angle);
+    void setYRotation(int angle);
+    void setZRotation(int angle);
+
+signals:
+    // signaling rotation from mouse movement
+    void xRotationChanged(int angle);
+    void yRotationChanged(int angle);
+    void zRotationChanged(int angle);
+
+private:
+    void draw();
+
+    int xRot;
+    int yRot;
+    int zRot;
+
+    QPoint lastPos;
+};
+
+#endif // MYGLWIDGET_H
+
